a3abf602dfccec7fc6e3053d9ee06e4e8f9b53bc
Christian Onwe <chrisoftech@gmail.com>
2022-08-16 10:55:21 +0000

    updated set-env parameters.

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export POSTGRES_PASSWORD=87881103
+export POSTGRES_HOST=cd0354-project-dev.cf9u21m27jta.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=cd0354_project_dev
+export AWS_BUCKET=cd0354-project-305639555920-dev-1
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
